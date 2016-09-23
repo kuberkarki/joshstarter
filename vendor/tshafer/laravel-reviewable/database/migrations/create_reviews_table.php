f925fe84a20926aaebfca5f4121b9bb19f44c266
Kuber Karki <karki.kuber@gmail.com>
2016-09-23 06:14:55 +0545

    searchable change

---
+++ vendor/tshafer/laravel-reviewable/database/migrations/create_reviews_table.php
@@ -0,0 +1,29 @@
+<?php
+
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+
+    /**
+     * Class CreateReviewsTable.
+     */
+    class CreateReviewsTable extends Migration
+    {
+
+        public function up()
+        {
+            Schema::create( 'reviews', function ( Blueprint $table ) {
+                $table->increments( 'id' );
+                $table->string( 'title' );
+                $table->text( 'body' );
+                $table->integer( 'rating' );
+                $table->morphs( 'reviewable' );
+                $table->morphs( 'author' );
+                $table->timestamps();
+            } );
+        }
+
+        public function down()
+        {
+            Schema::dropIfExists( 'reviews' );
+        }
+    }
---
+++ vendor/tshafer/laravel-reviewable/src/Traits/Reviewable.php
@@ -0,0 +1,58 @@
+<?php
+
+    namespace Tshafer\Reviewable\Traits;
+
+    use Illuminate\Database\Eloquent\Model;
+    use Tshafer\Reviewable\Models\Review;
+
+
+    /**
+     * Class Reviewable
+     *
+     * @package Tshafer\Reviewable\Traits
+     */
+    trait Reviewable
+    {
+
+        /**
+         * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+         */
+        public function reviews()
+        {
+            return $this->morphMany( Review::class, 'reviewable' );
+        }
+
+        /**
+         * @param            $data
+         * @param Model      $author
+         * @param Model|null $parent
+         *
+         * @return static
+         */
+        public function review( $data, Model $author, Model $parent = null )
+        {
+            return ( new Review() )->createReview( $this, $data, $author );
+        }
+
+        /**
+         * @param            $id
+         * @param            $data
+         * @param Model|null $parent
+         *
+         * @return mixed
+         */
+        public function updateReview( $id, $data, Model $parent = null )
+        {
+            return ( new Review() )->updateReview( $id, $data );
+        }
+
+        /**
+         * @param $id
+         *
+         * @return mixed
+         */
+        public function deleteReview( $id )
+        {
+            return ( new Review() )->deleteReview( $id );
+        }
+    }
