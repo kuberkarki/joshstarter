99ee4122e2e46a10bc1f372b01adc70c8b3b556e
Kuber Karki <karki.kuber@gmail.com>
2016-10-07 18:00:22 +0545

    booking details new migration and new view test

---
+++ resources/views/user/mybookings.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.eventday')
+
+{{-- Page title --}}
+@section('title')
+    Create event
+    @parent
+@stop
+
+{{-- page level styles --}}
+@section('header_styles')
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/jasny-bootstrap/css/jasny-bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/iCheck/css/minimal/blue.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/select2/css/select2-bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/datetimepicker/css/bootstrap-datetimepicker.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/frontend/user_account.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datetimepicker.css') }}" />
+
+@stop
+
+{{-- Page content --}}
+@section('content')
+List my Booked Ads <br/>
+List my Booked Tickets
+@stop
