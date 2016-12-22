868ca4c7c5578824201519de8e9538a2b09f0ca6
Kuber Karki <karki.kuber@gmail.com>
2016-12-22 13:44:12 +0545

    twitter api key regenerated

--- config/services.php
+++ config/services.php
@@ -46,8 +46,8 @@ return [
         'redirect'      => env('GOOGLE_REDIRECT','http://eventdayplanner.com/dev/public/auth/callback/google')
     ],
     'twitter' => [
-        'client_id'     => env('TWITTER_ID','LqYhaavfPT0AseUjU98QJzOWz'),
-        'client_secret' => env('TWITTER_SECRET','2apNDtElmF3MNRA1UvxgQAhazWpoNe0ngenLEzuyE8WASytn4'),
+        'client_id'     => env('TWITTER_ID','ZgsQvF0e408PZ4CO7Q69zeOxb'),
+        'client_secret' => env('TWITTER_SECRET','4I0RfrpN1RKWoSRTg5DbZYbCpTwhmzgIeMIdVXGV0r2onDe5pq'),
         'redirect'      => env('TWITTER_REDIRECT','http://eventdayplanner.com/dev/public/auth/callback/twitter')
     ],
     'linkedin' => [
